99cf4a9ed5b4e1879f0676c7589a17bb898ec813
Arnav Menon <arnavmenon@gmail.com>
2025-03-31 23:11:56 -0400

    updated main.tf for hadoop vm creation

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ terraform {
 }
 
 provider "openstack" {
-  auth_url    = "http://10.128.0.8/identity/v3"
+  auth_url    = "http://10.128.0.37/identity/v3"
   user_name   = "admin"
   password    = "nav+sanjeev"
   tenant_name = "admin"
@@ -16,8 +16,20 @@ provider "openstack" {
 
 resource "openstack_compute_instance_v2" "databag_instance" {
   name            = "databag-server"
-  image_name      = "Ubuntu 22.04" 
-  flavor_name     = "m1.small"      
+  image_name      = "Ubuntu 22.04 LTS" 
+  flavor_name     = "m1.medium"      
+  key_pair        = "databag-keypair"
+  security_groups = ["default"]
+
+  network {
+    name = "private"
+  }
+}
+
+resource "openstack_compute_instance_v2" "hadoop_instance" {
+  name            = "hadoop-server"
+  image_name      = "Ubuntu 22.04 LTS" 
+  flavor_name     = "m1.large"      
   key_pair        = "databag-keypair"
   security_groups = ["default"]
 
@@ -27,6 +39,10 @@ resource "openstack_compute_instance_v2" "databag_instance" {
 }
 
 resource "openstack_networking_floatingip_v2" "databag_ip" {
+  pool = "public"
+}
+
+resource "openstack_networking_floatingip_v2" "hadoop_ip" {
   pool = "public"
 }
 
